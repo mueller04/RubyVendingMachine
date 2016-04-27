5677ae1113f965cb1b7b8f4277bc34772dcc542b
Mike Mueller <mikemueller04@hotmail.com>
2016-04-27 15:01:27 -0400

    passed test_insert_dime_returns_10cents

--- machine.rb
+++ machine.rb
@@ -1,7 +1,11 @@
 class Machine
 
-  def insert(coin)
-    0.5
-  end
+    def insert(coin)
+      if coin == :nickel
+          return 0.5
+      else
+          return 0.1
+      end
+    end
 
 end
