e7c003b2f7599867057e66f4a7a17858ddaa7f8a
Mike Mueller <mikemueller04@hotmail.com>
2016-04-27 11:41:36 -0400

    test fails as no machine class

--- vendingmachinetest.rb
+++ vendingmachinetest.rb
@@ -1,2 +1,12 @@
 require_relative 'machine'
 require 'test/unit'
+
+class VendingMachineTest < Test::Unit::TestCase
+
+  def test_insert_nickel_return_5cents
+    machine = Machine.new
+    result = machine.insert(:nickel)
+    assert_equal(0.5, result)
+  end
+
+end
