13e9e79be014c2d3fe4898cd3ff5a61b21767c8a
Mike Mueller <mikemueller04@hotmail.com>
2016-04-27 15:02:26 -0400

    fixed error in nickel amount

--- machine.rb
+++ machine.rb
@@ -2,7 +2,7 @@ class Machine
 
     def insert(coin)
       if coin == :nickel
-          return 0.5
+          return 0.05
       else
           return 0.1
       end
--- vendingmachinetest.rb
+++ vendingmachinetest.rb
@@ -6,7 +6,7 @@ class VendingMachineTest < Test::Unit::TestCase
   def test_insert_nickel_return_5cents
     machine = Machine.new
     result = machine.insert(:nickel)
-    assert_equal(0.5, result)
+    assert_equal(0.05, result)
   end
 
   def test_insert_dime_returns_10cents
